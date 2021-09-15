2d6ef6ba175c59fa14ebd8f27dfe7e7bd73b05c6
Anzeron <Anzeron@163.com>
2021-09-15 23:05:23 +0800

    c++ code , add function template

---
+++ C++/01-Basics/Code/CPP01/template/FunctionTemplate1.cpp
@@ -0,0 +1,16 @@
+//
+//  FunctionTemplate1.cpp
+//  template
+//
+//  Created by jiayou on 2021/9/15.
+//
+
+#include "FunctionTemplate1.hpp"
+
+
+int add1(int a, int b)
+{
+    return a + b;
+}
+
+ 
---
+++ C++/01-Basics/Code/CPP01/template/FunctionTemplate1.hpp
@@ -0,0 +1,18 @@
+//
+//  FunctionTemplate1.hpp
+//  template
+//
+//  Created by jiayou on 2021/9/15.
+//
+
+#ifndef FunctionTemplate1_hpp
+#define FunctionTemplate1_hpp
+
+#include <stdio.h>
+
+int add1(int a, int b);
+
+template <typename  T>
+T add2(T a, T b);
+
+#endif /* FunctionTemplate1_hpp */
