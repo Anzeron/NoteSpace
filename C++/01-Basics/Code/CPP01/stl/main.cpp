7ca84ec7736be722d745cbbcb0977c86fad64737
Anzeron <Anzeron@163.com>
2021-11-02 21:12:35 +0800

    c++ code, add STL

---
+++ C++/01-Basics/Code/CPP01/stl/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  stl
+//
+//  Created by jiayou on 2021/10/31.
+//
+
+#include <iostream>
+#include "stl.hpp"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    test01();
+    return 0;
+}
---
+++ C++/01-Basics/Code/CPP01/stl/stl.hpp
@@ -0,0 +1,14 @@
+//
+//  stl.hpp
+//  stl
+//
+//  Created by jiayou on 2021/10/31.
+//
+
+#ifndef stl_hpp
+#define stl_hpp
+
+#include <stdio.h>
+
+void test01();
+#endif /* stl_hpp */
