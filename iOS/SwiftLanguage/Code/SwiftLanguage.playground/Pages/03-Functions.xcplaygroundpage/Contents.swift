31c888d3baa020ea2d34413644cd0b7c792d50ad
Anzeron <Anzeron@163.com>
2022-05-22 21:10:30 +0800

    iOS Swift Language code , add function type, inline

--- iOS/SwiftLanguage/Code/SwiftLanguage.playground/Pages/03-Functions.xcplaygroundpage/Contents.swift
+++ iOS/SwiftLanguage/Code/SwiftLanguage.playground/Pages/03-Functions.xcplaygroundpage/Contents.swift
@@ -2,6 +2,23 @@
 
 import Foundation
 
+func someFunction(a: Int = 1, b: Int ) {
+    
+}
+someFunction(a: 3, b: 6)
+someFunction(b: 4)
+
+func someFunction2(_ a: Int = 1, b: Int, _ c: Int = 2 ) {
+    
+}
+someFunction2(b: 1)
+
+@inline(never) func test() -> Void {
+    
+}
+
+
+
 // Variadic Parameters
 func arithmeticMean(_ numbers: Double...) -> Double {
     var total: Double = 0
@@ -48,6 +65,15 @@ func sum(_ numbers: Int...) -> Int {
 }
 
 sum(v1: 10, v2: 20)
+
+
+func chooseStepFunction(backward: Bool) -> (Int) -> Int {
+    func stepForward(input: Int) -> Int { return input + 1 }
+    func stepBackward(input: Int) -> Int { return input - 1 }
+    return backward ? stepBackward : stepForward
+}
+chooseStepFunction(backward: true)(2)
+chooseStepFunction(backward: false)(1)
 
 
 //: [Next](@next)
