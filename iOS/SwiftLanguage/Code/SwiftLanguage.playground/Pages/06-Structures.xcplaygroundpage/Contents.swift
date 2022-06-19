568539016c849085198f4684a4a9e3ff13be76f4
Anzeron <Anzeron@163.com>
2022-06-19 22:19:07 +0800

    iOS Swift Language code , add Class init

--- iOS/SwiftLanguage/Code/SwiftLanguage.playground/Pages/06-Structures.xcplaygroundpage/Contents.swift
+++ iOS/SwiftLanguage/Code/SwiftLanguage.playground/Pages/06-Structures.xcplaygroundpage/Contents.swift
@@ -35,6 +35,35 @@ struct Point4 {
 let p8 = Point4(x:1, y: 2)
 // let p9 = Point4(y: 2) 编译报错
 
+struct Point5 {
+    var x: Int = 0
+    var y: Int = 0
+}
+let p10 = Point5()
+
+struct Point6 {
+    var x: Int
+    var y: Int
+    init() {
+        x = 0
+        y = 0
+    }
+}
+let p11 = Point6()
+
+//class Point7 {
+//    var x: Int
+//    var y: Int
+//}
+//let p12 = Point7(x: 1, y: 2)
+
+class Point8 {
+    var x: Int = 1
+    var y: Int = 2
+}
+let p12 = Point8()
+//let p13 = Point8(x: 1, y: 2)
+
 
 
 //: [Next](@next)
