fa6eb7c9a55c14ebdf609d699e5ece57c1d9f4ac
Anzeron <Anzeron@163.com>
2022-07-13 12:54:13 +0800

    update Swift code Structures

--- iOS/SwiftLanguage/Code/SwiftLanguage.playground/Pages/06-Structures.xcplaygroundpage/Contents.swift
+++ iOS/SwiftLanguage/Code/SwiftLanguage.playground/Pages/06-Structures.xcplaygroundpage/Contents.swift
@@ -64,6 +64,46 @@ class Point8 {
 let p12 = Point8()
 //let p13 = Point8(x: 1, y: 2)
 
+class_getInstanceSize(Point8.self)
+let p14 = p12
+
+if p14 === p12 {
+    print("p14 === p12")
+}
+
+struct Point9 {
+    var x: Int
+    var b1: Bool
+    var b2: Bool
+    var y: Int
+}
+MemoryLayout<Point9>.size  // 24
+MemoryLayout<Point9>.stride    // 24
+MemoryLayout<Point9>.alignment     // 8
+
+class Point10 {
+    var x: Int
+    var b1: Bool
+    var b2: Bool
+    var y: Int
+    init(x: Int, b1: Bool, b2: Bool, y: Int) {
+        self.x = x
+        self.b1 = b1
+        self.b2 = b2
+        self.y = y
+    }
+}
+MemoryLayout<Point10>.size  // 8
+MemoryLayout<Point10>.stride    // 8
+MemoryLayout<Point10>.alignment     // 8
+
+let p15 = Point10(x: 1, b1: true, b2: false, y: 2)
+MemoryLayout.size(ofValue: p15)     // 8
+MemoryLayout.stride(ofValue: p15)   // 8
+MemoryLayout.alignment(ofValue: p15)    // 8
+
+
+
 
 
 //: [Next](@next)
